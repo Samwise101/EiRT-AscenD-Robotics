68dc9c626d58be6fbe727daab44793f283867cdb
Samwise101 <samuel.palenik20@gmail.com>
2025-09-20 09:27:43 +0200

    ROS2 publishing now working with Qt signals

--- ros2_ws/src/hmi/include/mainwindow.h
+++ ros2_ws/src/hmi/include/mainwindow.h
@@ -4,11 +4,13 @@
 #include <QMainWindow>
 #include "ui_mainwindow.h"
 #include <QTimer>
+#include <QString>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include <memory>
 #include <thread>
+
 
 class MainWindow : public QMainWindow
 {
@@ -23,6 +25,9 @@ private:
     std::shared_ptr<rclcpp::Node> node_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
     std::thread ros_thread_;
+
+private slots:
+    void push_button1_on_clicked(bool);
 };
 
 #endif // MAINWINDOW_H
--- ros2_ws/src/hmi/include/ui_mainwindow.h
+++ ros2_ws/src/hmi/include/ui_mainwindow.h
@@ -10,10 +10,14 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
+#include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -22,7 +26,14 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QPushButton *pushButton1;
+    QPushButton *pushButton_2;
     QMenuBar *menubar;
+    QMenu *menuFile;
+    QMenu *menuEdit;
+    QMenu *menuHelp;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
@@ -32,16 +43,43 @@ public:
         MainWindow->resize(800, 600);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayoutWidget = new QWidget(centralwidget);
+        verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(240, 130, 160, 80));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        pushButton1 = new QPushButton(verticalLayoutWidget);
+        pushButton1->setObjectName(QString::fromUtf8("pushButton1"));
+
+        verticalLayout->addWidget(pushButton1);
+
+        pushButton_2 = new QPushButton(verticalLayoutWidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+
+        verticalLayout->addWidget(pushButton_2);
+
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
         menubar->setGeometry(QRect(0, 0, 800, 22));
+        menuFile = new QMenu(menubar);
+        menuFile->setObjectName(QString::fromUtf8("menuFile"));
+        menuEdit = new QMenu(menubar);
+        menuEdit->setObjectName(QString::fromUtf8("menuEdit"));
+        menuHelp = new QMenu(menubar);
+        menuHelp->setObjectName(QString::fromUtf8("menuHelp"));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
         MainWindow->setStatusBar(statusbar);
 
+        menubar->addAction(menuFile->menuAction());
+        menubar->addAction(menuEdit->menuAction());
+        menubar->addAction(menuHelp->menuAction());
+
         retranslateUi(MainWindow);
+        QObject::connect(pushButton1, SIGNAL(clicked(bool)), MainWindow, SLOT(push_button1_on_clicked(bool)));
 
         QMetaObject::connectSlotsByName(MainWindow);
     } // setupUi
@@ -49,6 +87,11 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton1->setText(QCoreApplication::translate("MainWindow", "Hello", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "Test", nullptr));
+        menuFile->setTitle(QCoreApplication::translate("MainWindow", "File", nullptr));
+        menuEdit->setTitle(QCoreApplication::translate("MainWindow", "Edit", nullptr));
+        menuHelp->setTitle(QCoreApplication::translate("MainWindow", "Help", nullptr));
     } // retranslateUi
 
 };
