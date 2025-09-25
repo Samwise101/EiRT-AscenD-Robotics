d600a167ba190bff5a466f4a580a9fa0cdb509e9
Filip Lobpreis <f.lobpreis@gmail.com>
2025-09-25 22:11:55 +0200

    topics: remove unused topic and sort
    The removed topic is
    DRONEHIVE_ADD_LANDING_POS_TOPIC = "/dronehive/add_landing_position"
    
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- ros2_ws/src/dronehive/dronehive/utils/topics.py
+++ ros2_ws/src/dronehive/dronehive/utils/topics.py
@@ -1,6 +1,9 @@
-DRONEHIVE_NEW_BOX_TOPIC = "/dronehive/new_box"
+# Uninitialised state
 DRONEHIVE_NEW_BOX_CONFIMED_TOPIC = "/dronehive/new_box_confirmed"
+DRONEHIVE_NEW_BOX_TOPIC = "/dronehive/new_box"
+
+# Initialised state
 DRONEHIVE_DEINITIALISE_BOX_TOPIC = "/dronehive/deinitialise_box"
-DRONEHIVE_ADD_LANDING_POS_TOPIC = "/dronehive/add_landing_position"
-DRONEHIVE_REQUEST_LANDING_POS_TOPIC = "/dronehive/request_landing_pos"
+# TODO: Change the drone topic to be a function that returns the topic based on the drone ID
 DRONEHIVE_DRONE_TOPIC = "/dronehive/drone"
+DRONEHIVE_REQUEST_LANDING_POS_TOPIC = "/dronehive/request_landing_pos"
