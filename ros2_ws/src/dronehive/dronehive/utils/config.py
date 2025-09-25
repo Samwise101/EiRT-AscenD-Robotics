61d3eb30057fbb680a24b8bc5657a60773229471
Filip Lobpreis <f.lobpreis@gmail.com>
2025-09-25 21:20:09 +0200

    utils: PosstitionMessage needs to be created via default_factory
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- ros2_ws/src/dronehive/dronehive/utils/config.py
+++ ros2_ws/src/dronehive/dronehive/utils/config.py
@@ -1,6 +1,7 @@
 from dronehive_interfaces.msg import PositionMessage
+from rclpy.logging import get_logger as rclpy_logger
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
 import json
 
@@ -18,14 +19,17 @@ class Config:
 	box_id: str = ""
 	drone_id: str = ""
 	initialised: bool = False
-	lending_position: PositionMessage = PositionMessage()
+	lending_position: PositionMessage = field(default_factory=PositionMessage)
 
 	@staticmethod
 	def load() -> 'Config':
 		json_path = DRONEHIVE_CONFIG_DIR / "config.json"
+		rclpy_logger("Config").info(f"Loading configuration from {json_path}")
+
 		if json_path.exists():
 			with open(json_path, 'r') as f:
 				data = json.load(f)
+				rclpy_logger("Config").info(f"Configuration loaded: {data}")
 				return Config(
 					box_id = data.get("box_id", ""),
 					drone_id = data.get("drone_id", ""),
