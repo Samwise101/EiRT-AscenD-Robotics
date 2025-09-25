30e12d943cf978527b321097eff5c996745d43cf
Filip Lobpreis <f.lobpreis@gmail.com>
2025-09-25 21:35:54 +0200

    master_box: deinitiate box if requested
    This sets the box to setup state.
    
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- ros2_ws/src/dronehive/dronehive/utils/__init__.py
+++ ros2_ws/src/dronehive/dronehive/utils/__init__.py
@@ -1,6 +1,7 @@
 from .topics import *
 from .config import Config, DRONEHIVE_DIRS
 from .initialiser import Initialiser
+from typing import Union
 
 def dronehive_initialise() -> Config:
 	print("Initializing the package with constants:")
@@ -11,8 +12,10 @@ def dronehive_initialise() -> Config:
 	conf: Config = Config.load()
 	return conf
 
-def dronehive_deinitialise() -> None:
-	config: Config = Config.load()
+def dronehive_deinitialise(config: Union[Config, None] = None) -> None:
+	if config is None:
+		config = Config.load()
+
 	if config.initialised:
 		config.initialised = False
 		dronehive_update_config(config)
