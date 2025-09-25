f4dae2ace6638c44f854f763eb9e054d490f4c4f
Filip Lobpreis <f.lobpreis@gmail.com>
2025-09-25 21:35:11 +0200

    topics: add a topic to deinitiate a box
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- ros2_ws/src/dronehive/dronehive/utils/topics.py
+++ ros2_ws/src/dronehive/dronehive/utils/topics.py
@@ -1,5 +1,6 @@
 DRONEHIVE_NEW_BOX_TOPIC = "/dronehive/new_box"
 DRONEHIVE_NEW_BOX_CONFIMED_TOPIC = "/dronehive/new_box_confirmed"
+DRONEHIVE_DEINITIALISE_BOX_TOPIC = "/dronehive/deinitialise_box"
 DRONEHIVE_ADD_LANDING_POS_TOPIC = "/dronehive/add_landing_position"
 DRONEHIVE_REQUEST_LANDING_POS_TOPIC = "/dronehive/request_landing_pos"
 DRONEHIVE_DRONE_TOPIC = "/dronehive/drone"
