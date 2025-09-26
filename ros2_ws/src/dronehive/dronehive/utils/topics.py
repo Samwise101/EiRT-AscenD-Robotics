299d2b6bf0f486cb69a502315d018afdc4129349
Filip Lobpreis <f.lobpreis@gmail.com>
2025-09-26 19:21:11 +0200

    topics: devide topics, services and actions
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- ros2_ws/src/dronehive/dronehive/utils/topics.py
+++ ros2_ws/src/dronehive/dronehive/utils/topics.py
@@ -3,7 +3,13 @@ DRONEHIVE_NEW_BOX_CONFIMED_TOPIC = "/dronehive/new_box_confirmed"
 DRONEHIVE_NEW_BOX_TOPIC = "/dronehive/new_box"
 
 # Initialised state
+
+# Messages
 DRONEHIVE_DEINITIALISE_BOX_TOPIC = "/dronehive/deinitialise_box"
 # TODO: Change the drone topic to be a function that returns the topic based on the drone ID
 DRONEHIVE_DRONE_TOPIC = "/dronehive/drone"
 DRONEHIVE_REQUEST_LANDING_POS_TOPIC = "/dronehive/request_landing_pos"
+
+# Services
+
+# Actions
