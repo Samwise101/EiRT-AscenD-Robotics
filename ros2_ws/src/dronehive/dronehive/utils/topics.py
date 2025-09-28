38a9cf4ba335d06cd74e648cbead60f5fb1bafe8
Filip Lobpreis <f.lobpreis@gmail.com>
2025-09-29 00:05:13 +0200

    master_box: temporary publisher for drone landing operation
    So that we can do blocking state until the message is received we need
    to change the reliability to RELIABLE. Otherwise true would be returned
    imediately.
    
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- ros2_ws/src/dronehive/dronehive/utils/topics.py
+++ ros2_ws/src/dronehive/dronehive/utils/topics.py
@@ -1,16 +1,28 @@
-# Uninitialised state
+#######################
+# Uninitialised state #
+#######################
 DRONEHIVE_NEW_BOX_CONFIMED_TOPIC = "/dronehive/new_box_confirmed"
 DRONEHIVE_NEW_BOX_TOPIC = "/dronehive/new_box"
 
-# Initialised state
+#####################
+# Initialised state #
+#####################
 
-# Messages
+############
+# Messages #
+############
 DRONEHIVE_DEINITIALISE_BOX_TOPIC = "/dronehive/deinitialise_box"
-# TODO: Change the drone topic to be a function that returns the topic based on the drone ID
-DRONEHIVE_DRONE_TOPIC = "/dronehive/drone"
+
+# Topic that sends a request for landing position, the drone ID should be formatted in the topic.
+DRONEHIVE_DRONE_TOPIC = lambda drone_id: f"/dronehive/{drone_id}/drone"
+# Topic that receives a request for a drone to lend at a specific position.
 DRONEHIVE_REQUEST_LANDING_POS_TOPIC = "/dronehive/request_landing_pos"
 
-# Services
+############
+# Services #
+############
 DRONEHIVE_BOX_BROADCAST_SERVICE = "/dronehive/box_broadcast_service"
 
-# Actions
+###########
+# Actions #
+###########
