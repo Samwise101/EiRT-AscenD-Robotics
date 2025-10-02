bc7b43bfc3db0dd6845919ec48eb96f706434c20
MegaMagn <magni21@student.sdu.dk>
2025-10-02 14:36:36 +0200

    created drone_pos_pub and updated PoitionMessage

---
+++ ros2_ws/src/drone_tracking_node/src/drone_pos_pub.py
@@ -0,0 +1,37 @@
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from dronehive_interfaces.msg import PositionMessage
+from geometry_msgs.msg import PoseStamped
+
+class DronePositionPublisher(Node):
+	def __init__(self):
+		super().__init__('drone_pos_pub')
+		self.declare_parameter('drone_id', 'drone1')
+		self._drone_id = self.get_parameter('drone_id').get_parameter_value().string_value
+		self._pub = self.create_publisher(PositionMessage, '/drone_positions', 10) #might need to be changed if the GUI or masterbox expects it somewhere else
+		self._sub = self.create_subscription(
+			PoseStamped,
+			'/mavros/vision_pose/pose',
+			self.pose_callback,
+			10
+		)
+
+	def pose_callback(self, msg):
+		pos_msg = PositionMessage()
+        pos_msg.drone_id = self._drone_id
+		if hasattr(pos_msg, 'drone_id'):
+			pos_msg.drone_id = self._drone_id
+		pos_msg.x = msg.pose.position.x
+		pos_msg.y = msg.pose.position.y
+		pos_msg.z = msg.pose.position.z
+		self._pub.publish(pos_msg)
+
+def main(args=None):
+	rclpy.init(args=args)
+	node = DronePositionPublisher()
+	rclpy.spin(node)
+
+if __name__ == '__main__':
+	main()
