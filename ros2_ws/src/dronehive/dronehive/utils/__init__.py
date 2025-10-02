b247908d5c7ad041d1fc6e116e304fabc62208b2
Filip Lobpreis <f.lobpreis@gmail.com>
2025-10-02 19:01:49 +0200

    utils: add utm
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- ros2_ws/src/dronehive/dronehive/utils/__init__.py
+++ ros2_ws/src/dronehive/dronehive/utils/__init__.py
@@ -3,6 +3,7 @@ from .config import Config, DRONEHIVE_DIRS
 from .initialiser import Initialiser
 from .client import ServiceLessClient
 from .client_manager import ServiceClientManager
+from .utm import *
 
 from typing import Union
 from rclpy.logging import get_logger as rclpy_logger
