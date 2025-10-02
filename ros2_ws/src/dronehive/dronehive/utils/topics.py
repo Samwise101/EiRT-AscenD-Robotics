7738f29325a6a658ae148a7ae04219d127c1b42a
Filip Lobpreis <filob23@student.sdu.dk>
2025-10-02 13:17:05 +0200

    topics: new topics
    Signed-off-by: Filip Lobpreis <filob23@student.sdu.dk>

--- ros2_ws/src/dronehive/dronehive/utils/topics.py
+++ ros2_ws/src/dronehive/dronehive/utils/topics.py
@@ -2,6 +2,7 @@
 # Uninitialised state #
 #######################
 DRONEHIVE_NEW_BOX_CONFIMED_TOPIC = "/dronehive/new_box_confirmed"
+DRONEHIVE_NEW_SLAVE_BOX_CONFIMED_TOPIC = "/dronehive/new_slave_box_confirmed"
 DRONEHIVE_NEW_BOX_TOPIC = "/dronehive/new_box"
 
 #####################
@@ -12,9 +13,12 @@ DRONEHIVE_NEW_BOX_TOPIC = "/dronehive/new_box"
 # Messages #
 ############
 DRONEHIVE_DEINITIALISE_BOX_TOPIC = "/dronehive/deinitialise_box"
+DRONEHIVE_BOX_INITIALISE_TOPIC = "/dronehive/box_initialise"
+
+DRONEHIVE_DEINITIALISE_SLAVE_BOX_TOPIC = "/dronehive/deinitialise_slave_box"
 
 # Topic that sends a request for landing position, the drone ID should be formatted in the topic.
-DRONEHIVE_DRONE_LAND_REQUEST = lambda drone_id: f"/dronehive/{drone_id}/drone"
+DRONEHIVE_DRONE_LAND_REQUEST = "/dronehive/drone_land_request"
 # Topic that receives a request for a drone to lend at a specific position.
 DRONEHIVE_REQUEST_LANDING_POS_TOPIC = "/dronehive/request_landing_pos"
 
@@ -22,6 +26,7 @@ DRONEHIVE_REQUEST_LANDING_POS_TOPIC = "/dronehive/request_landing_pos"
 # Services #
 ############
 DRONEHIVE_BOX_BROADCAST_SERVICE = "/dronehive/box_broadcast_service"
+DRONEHIVE_DRONE_REQUESTED_LANDING_POS = "/dronehive/drone/landing_pos"
 
 ###########
 # Actions #
