31975b247ac7c0afd4058351d3446be2d5f424ec
Filip Lobpreis <filob23@student.sdu.dk>
2025-10-10 14:13:13 +0200

    landing: do not block until the landing position is received
    Signed-off-by: Filip Lobpreis <filob23@student.sdu.dk>

--- ros2_ws/src/drone_landing_traj_node/src/landing_traj_node.py
+++ ros2_ws/src/drone_landing_traj_node/src/landing_traj_node.py
@@ -41,7 +41,7 @@ class LandingTrajNode(Node):
         self.create_subscription(PoseStamped, '/mavros/local_position/pose', self.pose_cb, 10) #might need to be changed if the drone uses a different topic
 
         # --- Service client to the landing box ---
-        
+
         self.cli = self.create_client(DroneLandingService, dh.DRONEHIVE_DRONE_LAND_REQUEST)
         while not self.cli.wait_for_service(timeout_sec=2.0):
             self.get_logger().info(f'Waiting for {dh.DRONEHIVE_DRONE_LAND_REQUEST} service...')
@@ -75,6 +75,15 @@ class LandingTrajNode(Node):
 
     # ------------------------------------------------------------------
 
+    def future_callback(self, future):
+        if future.result() is not None:
+            landing_pos_msg = future.result().landing_pos
+            self.landing_pos = np.array([landing_pos_msg.lat, landing_pos_msg.lon, landing_pos_msg.elv])
+            self.get_logger().info(f"Received landing position: ({self.landing_pos[0]}, {self.landing_pos[1]}, {self.landing_pos[2]})")
+        else:
+            self.get_logger().warn('Landing service call failed or returned no data')
+
+
     def request_landing_position(self):
         """Request landing position from the master box node via DroneLandingService."""
         self.get_logger().info('Requesting landing position from master box...')
@@ -91,13 +100,7 @@ class LandingTrajNode(Node):
         # TODO: set req.drone_id when available
 
         future = self.cli.call_async(req)
-        rclpy.spin_until_future_complete(self, future)
-        if future.result() is not None:
-            landing_pos_msg = future.result().landing_pos
-            self.landing_pos = np.array([landing_pos_msg.lat, landing_pos_msg.lon, landing_pos_msg.elv])
-            self.get_logger().info(f"Received landing position: ({self.landing_pos[0]}, {self.landing_pos[1]}, {self.landing_pos[2]})")
-        else:
-            self.get_logger().warn('Landing service call failed or returned no data')
+        future.add_done_callback(self.future_callback)
 
     # ------------------------------------------------------------------
 
@@ -115,13 +118,13 @@ class LandingTrajNode(Node):
         a0 = np.zeros(3)
         t_accum = 0.0
         for i in range(len(self.waypoints)-1):
-            
+
             d = np.linalg.norm(self.waypoints[i+1] - self.waypoints[i])
             p0 = self.waypoints[i]
             p1 = self.waypoints[i+1]
             T = max(1.0, d / 0.5)  # slower descent: 0.5 m/s nominal
             t_accum += T
-            
+
             coeffs_xyz = []
             for axis in range(3):
                 coeffs = cubic_coeffs_from_boundary(p0[axis], p1[axis], v0[axis], a0[axis], T)
