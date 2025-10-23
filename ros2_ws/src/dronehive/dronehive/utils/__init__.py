db47da028edc565b9d4dfddeb70d667fcb24ac12
Filip Lobpreis <filob23@student.sdu.dk>
2025-10-23 10:38:12 +0200

    dynamixel: add the motor controller
    The added file is just the api for our code to move the motors.
    
    Signed-off-by: Filip Lobpreis <filob23@student.sdu.dk>

--- ros2_ws/src/dronehive/dronehive/utils/__init__.py
+++ ros2_ws/src/dronehive/dronehive/utils/__init__.py
@@ -3,6 +3,7 @@ from .config import Config, DRONEHIVE_DIRS
 from .initialiser import Initialiser
 from .client_manager import ServiceClientManager
 from .utm import *
+from .dynamixel_controller import XL430Controller
 
 from typing import Union
 from rclpy.logging import get_logger as rclpy_logger
