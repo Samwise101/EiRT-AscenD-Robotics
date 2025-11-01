b21c5f030aaf2be791b8395bc979edbf8b8238c0
Samwise101 <samuel.palenik20@gmail.com>
2025-11-01 16:18:36 +0100

    Fix remove build

--- build/hmi_backend/CMakeFiles/backend_node_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/backend_node_autogen"
-  "backend_node_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/backend_node_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/hmi_backend/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/samuel/EiRT-AscenD-Robotics/install/dronehive_interfaces/share/dronehive_interfaces/package.sh"
